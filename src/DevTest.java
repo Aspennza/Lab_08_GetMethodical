942c360af77d410e4612152c11a28bae21111a38
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-18 15:25:10 -0400

    getNonZeroLenString and getInt drafts complete; starting on getDouble

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,4 +1,63 @@
+import java.util.Scanner;
+
 public class DevTest
 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String userName = "";
+        int testInt = 0;
+
+        userName = getNonZeroLenString(in, "Please enter your username");
+        System.out.println("\nYour username is: " + userName + ".");
+
+        testInt = getInt(in, "Please enter an integer");
+        System.out.println("\nYour integer is: " + testInt + ".");
+    }
+
+    /**
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        do
+        {
+            System.out.print("\n" + prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+    }
+
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int retInt = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            if(pipe.hasNextInt()) {
+                retInt = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            }else {
+                trash = pipe.nextLine();
+                System.out.println("\nPlease enter an integer. You entered: " + trash + ".");
+            }
+        }while(!done);
+        return retInt;
+    }
+
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        double retDouble = 0.0;
+    }
 
 }
