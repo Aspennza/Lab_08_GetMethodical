5a4a441665df30da680592ab6391440650f3930a
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-22 14:37:12 -0400

    CheckOut draft complete

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -5,6 +5,16 @@ public class CheckOut
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        double itemPrice = 0.0;
+        double totalPrice = 0.0;
+        boolean notDone = true;
 
+        do {
+            itemPrice = SafeInput.getRangedDouble(in, "Enter the price of your item", 0.50, 10.00);
+            totalPrice = totalPrice + itemPrice;
+            notDone = SafeInput.getYNConfirm(in, "Would you like to continue inputting items?");
+        }while(notDone);
+
+        System.out.printf("\nThe total price of your items is $%3.2f.", totalPrice);
     }
 }
