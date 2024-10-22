7da6880d198e0e107d391a956c4a8674e9a1e853
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-22 13:40:20 -0400

    FavNumbers complete

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class FavNumbers
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        int favInt = 0;
+        double favDouble = 0.0;
 
+        favInt = SafeInput.getInt(in, "Enter your favorite integer");
+        System.out.println("\nYour favorite integer is " + favInt + ".");
+
+        favDouble = SafeInput.getDouble(in, "Enter your favorite double");
+        System.out.println("\nYour favorite double is " + favDouble + ".");
     }
 }
