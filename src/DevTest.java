1a12041013f46643984bf4d7367f798212a2c50e
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-22 13:15:02 -0400

    SafeInput methods complete

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//WRITE JAVADOC FOR EACH METHOD!
-
 public class DevTest
 {
     public static void main(String[] args)
@@ -12,24 +10,33 @@ public class DevTest
         double testDouble = 0.0;
         int testIntRanged = 0;
         double testDoubleRanged = 0.0;
+        boolean testBoolean = false;
+        String testRegEx = "";
 
-        //userName = getNonZeroLenString(in, "Please enter your username");
-        //System.out.println("\nYour username is: " + userName + ".");
+        userName = getNonZeroLenString(in, "Please enter your username");
+        System.out.println("\nYour username is: " + userName + ".");
 
-        //testInt = getInt(in, "Please enter an integer");
-        //System.out.println("\nYour integer is: " + testInt + ".");
+        testInt = getInt(in, "Please enter an integer");
+        System.out.println("\nYour integer is: " + testInt + ".");
 
-        //testDouble = getDouble(in, "Please enter a double");
-        //System.out.println("\nYour double is: " + testDouble + ".");
+        testDouble = getDouble(in, "Please enter a double");
+        System.out.println("\nYour double is: " + testDouble + ".");
 
-        //testIntRanged = getRangedInt(in, "Please enter an integer", 1, 10);
-        //System.out.println("\nYour ranged integer is: " + testIntRanged + ".");
+        testIntRanged = getRangedInt(in, "Please enter an integer", 1, 10);
+        System.out.println("\nYour ranged integer is: " + testIntRanged + ".");
 
         testDoubleRanged = getRangedDouble(in, "Please enter a double", 1.0, 10.0);
         System.out.println("\nYour ranged double is: " + testDoubleRanged + ".");
+
+        testBoolean = getYNConfirm(in, "Please enter Y or N");
+        System.out.println("\nYour boolean is: " + testBoolean + ".");
+
+        testRegEx = getRegExString(in, "Please enter your email address", "[a-zA-Z0-9]+@[a-zA-Z0-9]+[\\.][a-z]+");
+        System.out.println("\nYour regEx answer is: " + testRegEx + ".");
     }
 
     /**
+     * Retrieves a String value from the user longer than 0 characters
      *
      * @param pipe a Scanner opened to read from System.in
      * @param prompt prompt for the user
@@ -47,6 +54,14 @@ public class DevTest
 
         return retString;
     }
+
+    /**
+     *Retrieves an int value from the user with no limitations on range
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return an integer value with no limitations on range
+     */
 
     public static int getInt(Scanner pipe, String prompt)
     {
@@ -69,6 +84,14 @@ public class DevTest
         return retInt;
     }
 
+    /**
+     *Retrieves a double value from the user with no limitations on range
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a double value with no limitations on range
+     */
+
     public static double getDouble(Scanner pipe, String prompt)
     {
         double retDouble = 0.0;
@@ -90,6 +113,16 @@ public class DevTest
         return retDouble;
     }
 
+    /**
+     *Retrieves a range-limited integer value from the user
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @param low the lowest value that the user can validly enter
+     * @param high the highest value that the user can validly enter
+     * @return an integer value within the range established by the programmer
+     */
+
     public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
     {
         int retRangedInt = 0;
@@ -106,15 +139,25 @@ public class DevTest
                 if(retRangedInt >= low && retRangedInt <= high) {
                     done = true;
                 }else {
-                    System.out.println("\nThe number you input is out of range. Please enter a number at or between " + low + " and " + high + ". You entered: " + retRangedInt + ".");
+                    System.out.println("\nThe number you input is out of range. Please enter a number at or between [" + low + "-" + high + "]. You entered: " + retRangedInt + ".");
                 }
             }else {
                 trash = pipe.nextLine();
-                System.out.println("\nPlease enter an integer at or between " + low + " and " + high + ". You entered: " + trash + ".");
+                System.out.println("\nPlease enter an integer at or between [" + low + "-" + high + "]. You entered: " + trash + ".");
             }
         }while(!done);
         return retRangedInt;
     }
+
+    /**
+     *Retrieves a range-limited double value from the user
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @param low the lowest value that the user can validly enter
+     * @param high the highest value that the user can validly enter
+     * @return a double value within the range established by the programmer
+     */
 
     public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
     {
@@ -133,16 +176,78 @@ public class DevTest
                 {
                     done = true;
                 }else {
-                    System.out.println("\nThe number you input is out of range. Please enter a number at or between " + low + " and " + high + ". You entered: " + retRangedDouble + ".");
+                    System.out.println("\nThe number you input is out of range. Please enter a number at or between [" + low + "-" + high + "]. You entered: " + retRangedDouble + ".");
                 }
             }else {
                 trash = pipe.nextLine();
-                System.out.println("\nPlease enter a double at or between " + low + " and " + high + ". You entered: " + trash + ".");
+                System.out.println("\nPlease enter a double at or between [" + low + "-" + high + "]. You entered: " + trash + ".");
             }
         }while(!done);
         return retRangedDouble;
     }
 
-    //come back for getYNConfirm
+    /**
+     *Retrieves a Y or N String value from the user and produces a true or false boolean value
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a boolean value (true/false) corresponding to the Y or N answer provided by the user
+     */
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt)
+    {
+        String yesNo = "";
+        boolean retBoolean = true;
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + " [Y/N]: ");
+            yesNo = pipe.nextLine();
+            if(yesNo.equalsIgnoreCase("Y"))
+            {
+                retBoolean = true;
+                done = true;
+            }else if(yesNo.equalsIgnoreCase("N"))
+            {
+                retBoolean = false;
+                done = true;
+            }else
+            {
+                System.out.println("\nPlease enter Y or N [Y/N]. You entered: " + yesNo + ".");
+            }
+        }while(!done);
+        return retBoolean;
+    }
+
+    /**
+     *Retrieves a String value from the user adhering to the regEx pattern provided by the programmer
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @param regEx the regEx String that the user's input must follow
+     * @return a String that follows the regEx pattern provided
+     */
+
+    public static String getRegExString(Scanner pipe, String prompt, String regEx)
+    {
+        String userInput = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            userInput = pipe.nextLine();
+
+            if(userInput.matches(regEx))
+            {
+                done = true;
+            }else
+            {
+                System.out.println("\nPlease enter an answer matching the regEx pattern: " + regEx + ". You entered: " + userInput + ".");
+            }
+        }while(!done);
+        return userInput;
+    }
 
 }
